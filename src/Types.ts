ffb5642e4201ec85d8e0c4a7c898dba13b89ec1f
Chavez Harris <codedbychavez@gmail.com>
2023-10-10 17:45:58 -0300

    Add configcat-plugin

---
+++ src/Types.ts
@@ -0,0 +1,12 @@
+import { PollingMode } from "configcat-common";
+import { IVueAutoPollOptions, IVueLazyLoadingOptions, IVueManualPollOptions } from ".";
+
+type ConfigCatPluginOptions = {
+    sdkKey: string;
+    pollingMode?: PollingMode;
+    clientOptions: IVueAutoPollOptions | IVueManualPollOptions | IVueLazyLoadingOptions
+}
+
+
+
+export type { ConfigCatPluginOptions };
---
+++ src/plugins/ConfigCatPlugin.ts
@@ -0,0 +1,39 @@
+import * as configcat from 'configcat-common';
+import { HttpConfigFetcher } from './ConfigFetcher';
+import { LocalStorageCache } from './LocalStorageCache';
+import CONFIGCAT_SDK_VERSION from './Version';
+import type { ConfigCatPluginOptions } from '../Types';
+import type { IConfigCatKernel } from 'configcat-common';
+
+import type { App } from 'vue';
+
+import { PollingMode } from 'configcat-common';
+
+export default {
+    install: (app: App, options: ConfigCatPluginOptions): void => {
+        const { sdkKey, pollingMode, clientOptions } = options;
+        const configCatKernal: IConfigCatKernel = {
+            sdkType: 'ConfigCat-Vue',
+            sdkVersion: CONFIGCAT_SDK_VERSION,
+            configFetcher: new HttpConfigFetcher(),
+            defaultCacheFactory: options => new configcat.ExternalConfigCache(new LocalStorageCache(), options.logger)
+        }
+
+        const client = configcat.getClient(
+            sdkKey,
+            pollingMode ?? PollingMode.AutoPoll,
+            clientOptions,
+            configCatKernal
+        );
+
+        const configCat = { client };
+
+        app.config.globalProperties.$configcat = configCat;
+
+        const originalAppUnmount = app.unmount;
+        app.unmount = function () {
+            originalAppUnmount.apply(arguments);
+            client.dispose();
+        }
+    }
+}
